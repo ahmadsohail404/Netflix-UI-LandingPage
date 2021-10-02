49524d07f06843e862d182352dc706c91725d91f
Hrithik Raj <iamhrithikraj26@gmail.com>
2021-10-03 05:07:56 +0530

    added automatic closing of accordian; closes #7

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-
-const question = document.querySelectorAll('.question');
-question.forEach((ques)=>{
-  ques.addEventListener('click',(e)=>{
-    const i =ques.lastElementChild.firstElementChild;
-    const ans=ques.nextElementSibling;
-    ans.classList.toggle('show');
-    i.classList.toggle('rotate');
-  })
+const question = document.querySelectorAll(".question");
+question.forEach((ques) => {
+  ques.addEventListener("click", (e) => {
+    /* closes previously opened FAQ */
+    question.forEach((opened_ques) => {
+      const opened_ans = opened_ques.nextElementSibling;
+      opened_ans.classList.remove("show");
+    });
+    const i = ques.lastElementChild.firstElementChild;
+    const ans = ques.nextElementSibling;
+    ans.classList.toggle("show");
+    i.classList.toggle("rotate");
+  });
 });
