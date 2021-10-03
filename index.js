ed7efded9c3d49e3bc9ed7c36dc87e08e79286b9
Sudev Suresh S <sudevssuresh@gmail.com>
2021-10-03 15:20:23 +0530

    Moved index.js from root to js folder

--- index.js
+++
@@ -1,9 +0,0 @@
-/* Loading Screen logic */
-window.onload = () => {
-  document.querySelector(".loading-screen").style =
-    " animation: load-remove 1s forwards;";
-  document.querySelector("html").removeAttribute("style");
-  setTimeout(() => {
-    document.querySelector(".loading-screen").remove();
-  }, 5000);
-};
