70c3b6e3360ceb798a0aab5c1b00eb2bbb5a5254
Sudev Suresh S <sudevssuresh@gmail.com>
2021-10-03 12:55:47 +0530

    Added loading function to js/index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+/* Loading Screen logic */
+window.onload = () => {
+  document.querySelector(".loading-screen").style =
+    " animation: load-remove 1s forwards;";
+  document.querySelector("html").removeAttribute("style");
+  setTimeout(() => {
+    document.querySelector(".loading-screen").remove();
+  }, 5000);
+};
+
 const question = document.querySelectorAll(".question");
 question.forEach((ques) => {
   ques.addEventListener("click", (e) => {
