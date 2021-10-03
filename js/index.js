2fb02a93c789b2b90cc80119ac5c4b45d9a4a269
muskan <59757988+Muskan-creator@users.noreply.github.com>
2021-10-03 14:10:17 +0530

    improved faq section

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-const question = document.querySelectorAll(".question");
-question.forEach((ques) => {
-  ques.addEventListener("click", (e) => {
-    /* closes previously opened question */
-    question.forEach((opened_ques) => {
-      const opened_ans = opened_ques.nextElementSibling;
-      opened_ans.classList.remove("show");
-    });
-    const i = ques.lastElementChild.firstElementChild;
-    const ans = ques.nextElementSibling;
-    ans.classList.toggle("show");
-    i.classList.toggle("rotate");
-  });
-});
+
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
