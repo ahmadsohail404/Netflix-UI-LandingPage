5c3eb65e7b8f9dd18544dd829d00d5bda547198f
Sudev Suresh S <sudevssuresh@gmail.com>
2021-10-03 12:45:16 +0530

    Added loading screen with animation

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+/* Loading Screen logic */
+window.onload = () => {
+  document.querySelector(".loading-screen").style =
+    " animation: load-remove 1s forwards;";
+  document.querySelector("html").removeAttribute("style");
+  setTimeout(() => {
+    document.querySelector(".loading-screen").remove();
+  }, 5000);
+};
 
-const question = document.querySelectorAll('.question');
-question.forEach((ques)=>{
-  ques.addEventListener('click',(e)=>{
-    const i =ques.lastElementChild.firstElementChild;
-    const ans=ques.nextElementSibling;
-    ans.classList.toggle('show');
-    i.classList.toggle('rotate');
-  })
+const question = document.querySelectorAll(".question");
+question.forEach((ques) => {
+  ques.addEventListener("click", (e) => {
+    const i = ques.lastElementChild.firstElementChild;
+    const ans = ques.nextElementSibling;
+    ans.classList.toggle("show");
+    i.classList.toggle("rotate");
+  });
 });
