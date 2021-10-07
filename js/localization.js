d15616d1d3c4e0f2bc998c8470cbc7f89d91f899
Hrithik Raj <iamhrithikraj26@gmail.com>
2021-10-08 02:47:19 +0530

    added localization with a support of hindi language

---
+++ js/localization.js
@@ -0,0 +1,128 @@
+let lang = {
+  english: {
+    india: "Netflix India",
+    "ul-1-a1": "FAQ",
+    "ul-1-a2": "Investor Relation",
+    "ul-1-a3": "Privacy",
+    "ul-1-a4": "Speed Test",
+    "ul-1-a5": "Helps",
+    "ul-1-a6": "Jobs",
+    "ul-1-a7": "Cookies Prefernace",
+    "ul-1-a8": "Legal Notice",
+    "ul-2-a1": "Account",
+    "ul-2-a2": "Ways to Watch",
+    "ul-2-a3": "Corporate Information",
+    "ul-2-a4": "Netflix Originals",
+    "ul-2-a5": "Media Centers",
+    "ul-2-a6": "Terms of Uses",
+    "ul-2-a7": "Contact Us",
+    "footer-p": 'Questions? Call 000-800-040-1843',
+    "email-btn-2": "Get started",
+    "text-p1":
+      "Ready to watch? Enter your email to create or restart your membership.",
+    "sec-5-span-5": "How do I cancel?",
+    "sec-5-p-5":
+      "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+    "sec-5-span-4": "Where can I watch?",
+    "sec-5-p-4":
+      "Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+    "sec-5-span-3": "What I can Watch on Netflix?",
+    "sec-5-p-3":
+      "Netflix has an extensive library of feature films,documentaries,TV, shows, anime, award-winning Netflix originals,and more. Watch as much as you want, anytime you want.",
+    "sec-5-span-2": "How much dose netflix costs?",
+    "sec-5-p-2":
+      " Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 199 to ₹ 799 a month. No extra costs, no contracts.",
+    "sec-5-span-1": "What is NetFlix?",
+    "sec-5-p-1":
+      "   Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.<br /> You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!",
+    "sec-5-h1": "Frequently Asked Questions",
+    "download-text-h4":
+      "Send children on adventures with their favourite characters in a space made just for them—free with your membership.",
+    "download-text-h1": "Create profiles for childern.",
+    "sec-4-h4":
+      " Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+    "sec-4-h1": "Watch everywhere.",
+    "sec-3-h4":
+      "Save your favourites easily and always have something to watch.",
+    "sec-3-h1": "Download your shows to watch offline.",
+    "sec-2-h4":
+      "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    "sec-2-h1": "Enjoy on your TV.",
+    "email-btn": "Get started",
+    "main-text-h5":
+      " Ready to watch? Enter your email to create or restart your membership.",
+    "main-text-h4": "Watch anywhere. Cancel anytime.",
+    "main-text-h2": "shows and more.",
+    "main-text-h1": "Unlimited movies, TV",
+    "sign-in-btn": "Sign In",
+  },
+  hindi: {
+    india: "Netflix भारत",
+    "ul-1-a1": "FAQ",
+    "ul-1-a2": "इंवेस्टर संबंध",
+    "ul-1-a3": "प्रायवेसी",
+    "ul-1-a4": "स्पीड टेस्ट",
+    "ul-1-a5": "सहायता केंद्र",
+    "ul-1-a6": "नौकरियां",
+    "ul-1-a7": "कुकी प्रेफ़रेंस",
+    "ul-1-a8": "कानूनी सूचनाएं",
+    "ul-2-a1": "अकाउंट",
+    "ul-2-a2": "देखने के तरीके",
+    "ul-2-a3": "कॉरपोरेट जानकारी",
+    "ul-2-a4": "सिर्फ़ Netflix पर",
+    "ul-2-a5": "मीडिया केंद्र",
+    "ul-2-a6": "उपयोग की शर्तें",
+    "ul-2-a7": "हमसे संपर्क करें",
+    "footer-p": "कोई सवाल है? 000-800-040-1843 पर कॉल करें",
+    "email-btn-2": "शुरू करें",
+    "text-p1":
+      "देखने के लिए तैयार हैं? अपनी मेंबरशिप बनाने या रीस्टार्ट करने के लिए अपना ईमेल एड्रेस डालें.",
+    "sec-5-span-5": "मैं कैसे कैंसल करूं?",
+    "sec-5-p-5":
+      "Netflix लचीला है. कोई परेशान करने वाले कॉन्ट्रैक्ट नहीं और कोई बंधन नहीं हैं. आप आसानी से दो क्लिक में अपना अकाउंट ऑनलाइन कैंसल कर सकते हैं. कोई कैंसलेशन फ़ीस नहीं है – अपना अकाउंट कभी भी शुरू या बंद करें.",
+    "sec-5-span-4": "मैं कहां देख सकता हूं?",
+    "sec-5-p-4":
+      "कहीं भी, कभी भी देखें अपने पर्सनल कंप्यूटर से या स्मार्ट टीवी, स्मार्टफ़ोन, टैबलेट, स्ट्रीमिंग मीडिया प्लेयर और गेम कंसोल सहित Netflix ऐप ऑफ़र करने वाले किसी भी इंटरनेट-कनेक्टेड डिवाइस पर तुरंत देखने के लिए, वेब पर netflix.com पर अपने Netflix अकाउंट में साइन इन करें. आप iOS, Android या Windows 10 ऐप से भी अपने पसंदीदा शो डाउनलोड कर सकते हैं. चलते-फिरते और बिना इंटरनेट कनेक्शन के देखने के लिए डाउनलोड का उपयोग करें. Netflix को अपने साथ कहीं भी ले जाएं.",
+    "sec-5-span-3": "मैं Netflix पर क्या देख सकता/सकती हूं?",
+    "sec-5-p-3":
+      "Netflix की बहुत बड़ी लाइब्रेरी में फ़ीचर फ़िल्मों, डॉक्यूमेंट्री, टीवी शो, ऐनिमे, अवॉर्ड-विनिंग Netflix ओरिजिनल्स के अलावा और भी बहुत कुछ है. आप जितना चाहें, कभी भी देखें.",
+    "sec-5-span-2": "Netflix की कीमत कितनी है?",
+    "sec-5-p-2":
+      "हर महीने की तय फ़ी देकर अपने स्मार्टफ़ोन, टैबलेट, स्मार्ट टीवी, लैपटॉप, या स्ट्रीमिंग डिवाइस पर Netflix देखें. हर महीने ₹199 से ₹799 तक के प्लान. कोई एक्सट्रा कीमत या कॉन्ट्रैक्ट नहीं.",
+    "sec-5-span-1": "Netflix क्या है?",
+    "sec-5-p-1":
+      "Netflix एक स्ट्रीमिंग सर्विस है जिसके ज़रिए आप हज़ारों इंटरनेट-कनेक्टेड डिवाइस पर तरह-तरह के अवॉर्ड विजेता टीवी शो, फ़िल्में, ऐनिमे, डॉक्यूमेंट्रीज़ का लुत्फ़ उठा सकते हैं. आप जब चाहें, जितना चाहें, बिना किसी विज्ञापन के देख सकते हैं – सारा कॉन्टेंट बहुत कम मासिक शुल्क पर. खोजने के लिए हमेशा कुछ नया होता है और हर हफ़्ते नए टीवी शो और फ़िल्में जोड़ी जाती हैं!",
+    "sec-5-h1": "अक्सर पूछे जाने वाले सवाल",
+    "download-text-h4":
+      "बच्चों को जाने दें अपने पसंदीदा किरदारों के साथ उस रोमांचक सफ़र पर जो सिर्फ़ उनके लिए ही है - आपकी मेंबरशिप के साथ फ़्री.",
+    "download-text-h1": "बच्चों के लिए प्रोफ़ाइल बनाएं.",
+    "sec-4-h4":
+      "बिना ज़्यादा पेमेंट किए, अपने फ़ोन, टैबलेट, लैपटॉप और टीवी पर अनलिमिटेड फ़िल्में और टीवी शो स्ट्रीम करें.",
+    "sec-4-h1": "हर जगह देखें.",
+    "sec-3-h4":
+      "अपने पसंदीदा शो और फ़िल्में सेव करें, ताकि आप कभी भी इन्हें देख सकें.",
+    "sec-3-h1": "ऑफ़लाइन देखने के लिए अपने शो डाउनलोड करें.",
+    "sec-2-h4":
+      "स्मार्ट टीवी, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray प्लेयर के साथ ही दूसरे डिवाइस पर भी देखें.",
+    "sec-2-h1": "अपनी टीवी पर मज़ा लें.",
+    "email-btn": "शुरू करें",
+    "main-text-h5":
+      "देखने के लिए तैयार हैं? अपनी मेंबरशिप बनाने या रीस्टार्ट करने के लिए अपना ईमेल एड्रेस डालें.",
+    "main-text-h4": "जहां चाहें देखें. जब चाहें कैंसल करें.",
+    "main-text-h2": "बहुत कुछ.",
+    "main-text-h1": "अनलिमिटेड फ़िल्में, टीवी शो और",
+    "sign-in-btn": "साइन इन करें",
+  },
+};
+
+document.getElementById("lang").addEventListener("change", (e) => {
+  const selected_lang = e.target.value;
+  const data = lang[selected_lang];
+  for (let element in data) {
+    if (data.hasOwnProperty(element)) {
+      content = data[element];
+      console.log(element, content);
+      document.getElementById(element).innerText = content;
+    }
+  }
+});
